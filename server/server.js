9808c05be5ad18794895f1b96997403afbc52505
Thiago Nogueira Justo <thiago.j.terceiro@brasilcap.com.br>
2020-01-22 14:56:38 -0300

    Adicionado dotenv na aplicação para variaveis de ambiente

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+require('dotenv/config');
+
 const express = require('express'),
     app = express();
 
-app.listen(3000, function() {
+app.listen(process.env.APP_LISTEN_PORT, function() {
      console.log('Servidor escutando na porta: ' + this.address().port)
 });
