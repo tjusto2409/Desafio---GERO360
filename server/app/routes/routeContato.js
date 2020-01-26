99f30dacf7eae9d611a8b973353c25b13bb1e04d
Thiago Justo <tjusto2409@gmail.com>
2020-01-26 09:24:51 -0300

    Inserido os metodos do controller na rota da aplicação

--- server/app/routes/routeContato.js
+++ server/app/routes/routeContato.js
@@ -1,24 +1,12 @@
+const contatosController = require('../controllers/ContatosController');
+
 const routeContato = function(router) {
     router
-        .get(function(req, res){
-            console.log(req.params.id);
-            res.json('Usuario ' + req.params.id)
-        })
-        .get('/:id', function(req, res){
-            console.log(req.params.id);
-            res.json('Usuario localizado ' + req.params.id)
-        })
-        .post('/', function(req, res){
-            res.json('Usuario cadastrado!');
-        })
-        .put('/:id', function(req, res){
-            console.log(req.params.id);
-            res.json('Usuario atualizado ' + req.params.id);
-        })
-        .delete('/:id', function(req, res){
-            console.log(req.params.id);
-            res.json('Usuario excluido ' + req.params.id);
-        });
+        .get('/', contatosController.getContatos)
+        .get('/:id', contatosController.getContatos)
+        .post('/', contatosController.saveContato)
+        .put('/:id', contatosController.updateContato)
+        .delete('/:id', contatosController.deleteContato);
 
     return router;
 }
