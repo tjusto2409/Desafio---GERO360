83c037183e56660f5173c5dc44b8903d54e694b9
Thiago Justo <tjusto2409@gmail.com>
2020-01-26 09:22:32 -0300

    Shared module para compartilhar componentes reutilizaveis

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
+
+import { InputFieldComponent } from './components/input-field/input-field.component';
+import { TableComponent } from './components/table/table.component';
+import { KeysPipe } from './pipes/keys.pipe';
+
+@NgModule({
+  declarations: [
+    InputFieldComponent,
+    TableComponent,
+    KeysPipe
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot()
+  ],
+  exports: [
+    InputFieldComponent,
+    TableComponent
+  ]
+})
+export class SharedModule { }
