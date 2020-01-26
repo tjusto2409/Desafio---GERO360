24c129180b1ae03c25f8f33ff9c952997fdea107
Thiago Justo <tjusto2409@gmail.com>
2020-01-26 09:28:02 -0300

    Service ja nao mais faz o teste de conexao, tudo fica na responsabilidade do connection factory

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 require('dotenv/config');
 
 const app = require('./app/config/express'),
-    connectionFactory = require('./app/infra/ConnectionFactory');
+    ConnectionFactory = require('./app/infra/ConnectionFactory');
 
 app.listen(process.env.APP_LISTEN_PORT, function() {
     console.log('Servidor escutando na porta: ' + this.address().port);
-    connectionFactory.testingConnection();
 });
